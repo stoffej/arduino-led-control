617162f0f0000fdd3be5c79aa6cb98a3064b5bd6
kristoffer johansson <kristoffer.c.johansson@gmail.com>
2021-10-24 19:08:48 +0200

    Update pyled.py

--- pyled.py
+++ pyled.py
@@ -1,3 +1,4 @@
+import re
 import serial.tools.list_ports
 import time
 import sys
@@ -15,6 +16,7 @@ def init():
     return serial_handler
 
 def set(serial_l,red,green,blue):
+    serial_l.write(("m 0\n").encode())
     cmd = f"c {red}.{green}.{blue}"
     serial_l.write((cmd + "\n").encode())
 
@@ -29,19 +31,23 @@ def main():
     ic(t_status)
     serial_l = init()
     if(serial_l is not None):
-        # if t_activity == "Away":
-        #     set(serial_l,255,200,0)
-        # elif t_activity == "Available":
-        #     set(serial_l,0,255,0)
-        if t_activity == "In a meeting" or t_activity == "In a call":
+        if t_status == "Away" or t_status == "Be right back" or t_status == "Offline":
+            serial_l.write(("b 0\n").encode())
+        elif t_activity == "In a meeting" or t_activity == "In a call":
+            serial_l.write(("b 255\n").encode())
+            time.sleep(1)
             set(serial_l,255,0,0)
+        elif t_activity == "calling":
+            serial_l.write(("b 255\n").encode())
+            time.sleep(1)
+            serial_l.write(("m 52\n").encode())
         else:
+            serial_l.write(("b 255\n").encode())
+            time.sleep(1)
             set(serial_l,0,255,0)
     else:
         print("no arduino board found")
     end(serial_l)
 
-# Using the special variable
-# __name__
 if __name__=="__main__":
     main()
